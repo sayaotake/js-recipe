282ff96d1b6218ac52491791c864e83c4cb9361b
sayaotake <saya42438@gmail.com>
2023-04-16 19:27:06 +0900

    ゼミでFizzBuzzを作りました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,12 +1,13 @@
  // 引数 number を受け取る関数
-        // eslint-disable-next-line no-unused-vars
-        const genkiFunction = function(number) {
-            // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
-            for (let n = 1; n <= number; n++) {
-                if (n % 3 === 0) {
-                console.log(`${n}!!!!!!!`)
-                } else {
-                console.log(n)
-                }
-            }
-            }
+
+// eslint-disable-next-line no-unused-vars
+const genkiFunction = function(number) {
+    // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+    for (let n = 1; n <= number; n++) {
+      if (n % 3 === 0) {
+        console.log(`${n}!!!!!!!`)
+      } else {
+        console.log(n)
+      }
+    }
+  }
